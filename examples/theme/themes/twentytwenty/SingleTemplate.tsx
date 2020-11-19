251b6f0617928db2af2cecc3dcc22df7113221a6
Matt Landers <matt@platypi.io>
2020-11-19 12:21:05 -0600

    added single template, headers

---
+++ examples/theme/themes/twentytwenty/SingleTemplate.tsx
@@ -0,0 +1,24 @@
+import { EntryHeader } from './components/EntryHeader';
+import { usePost } from '@wpengine/headless';
+
+export const SingleTemplate = () => {
+  const post = usePost('');
+
+  return (
+    <>
+      <main id="site-content" role="main">
+        {post && (
+          <article key={post.id} id={`post-${post.id}`}>
+            <div className="post-inner">
+              <EntryHeader title={post.title} single={true} />
+              <div
+                className="entry-content"
+                dangerouslySetInnerHTML={{ __html: post.excerpt }}
+              />
+            </div>
+          </article>
+        )}
+      </main>
+    </>
+  );
+};
