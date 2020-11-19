2fda88fceb1eec83ab289e1e13f0102dd3e4e94f
Matt Landers <matt@platypi.io>
2020-11-19 10:22:20 -0600

    removed unused items from example

--- examples/theme/pages/index.tsx
+++ examples/theme/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Layout } from '../lib/components';
+// import { Layout } from '../lib/components';
 
 export default function Home() {
-  return <Layout />;
+  return <h1>Test</h1>;
 }
